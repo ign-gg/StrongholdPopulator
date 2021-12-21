75daa8ec1eafe46e0ed3aa1bad2277dbbcc37df8
Petteri <26197131+PetteriM1@users.noreply.github.com>
2021-12-21 17:29:20 +0200

    Do not give empty filled maps

--- src/main/java/cn/wode490390/nukkit/shpop/loot/StrongholdLibraryChest.java
+++ src/main/java/cn/wode490390/nukkit/shpop/loot/StrongholdLibraryChest.java
@@ -19,7 +19,7 @@ public class StrongholdLibraryChest extends RandomizableContainer {
         PoolBuilder pool1 = new PoolBuilder()
                 .register(new ItemEntry(Item.BOOK, 0, 3, 100))
                 .register(new ItemEntry(Item.PAPER, 0, 7, 2, 100))
-                .register(new ItemEntry(Item.MAP, 5)) //TODO: EMPTY_MAP
+                .register(new ItemEntry(Item.EMPTY_MAP, 5))
                 .register(new ItemEntry(Item.COMPASS, 5))
                 .register(new ItemEntry(Item.ENCHANTED_BOOK, 60)); //TODO: treasure enchant_with_levels 30
         this.pools.put(pool1.build(), new RollEntry(10, 2, pool1.getTotalWeight()));
